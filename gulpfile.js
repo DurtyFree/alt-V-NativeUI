ea93889b5c6ce027bbd2e629b88fd61ddace3d45
GamingMaster <steven@fam-modl.de>
2018-12-04 11:32:05 +0100

    gulp-copy

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,18 @@ const gulp = require("gulp");
 
 const webpack = require("webpack-stream");
 
+const gulpCopy = require("gulp-copy");
+
+const path = require("path");
+
 gulp.task("build", function() {
 	return gulp
 		.src("index.ts")
 		.pipe(webpack(require("./webpack.config")))
-		.pipe(gulp.dest("dist"));
+		.pipe(gulp.dest("dist"))
+		.pipe(
+			gulpCopy(path.resolve("../../server-files/client_packages/nativeui"), {
+				prefix: 1
+			})
+		);
 });
